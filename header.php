6da6f70c96f1446ac82b2576238e62122e113256
Valentin Mathieu <valentin.mathieu@laplateforme.io>
2021-12-09 22:15:11 +0100

    Header OK / Footer OK / Index en construction

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+
+    <a href="index.php"><img id="logo_header" width="182px" height="130px" src="Assets/logo_salle.png" alt="logo"></a>
+    <nav>
+        <a href="inscription.php">INSCRIPTION</a>
+        <a href="connexion.php">CONNEXION</a>
+        <a href="reservation.php">RESERVATION</a>
+        <a href="profil.php">PROFIL</a>
+        <a href="planning.php">PLANNING</a>
+        <div id="indicator"></div>
+    </nav>
+    
+</header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="index.css">
+  </head>
+<body>
+
+<?php require("header.php"); ?>
+
+<main>
+
+    
+
+
+</main>
+
+<?php require("footer.php"); ?>
