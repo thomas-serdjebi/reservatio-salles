0b0cca8b47acb1f3cdf06bfb72e9081207c4bea2
Valentin Mathieu <valentin.mathieu@laplateforme.io>
2021-12-11 13:35:58 +0100

    modif form reservation

--- reservation-form.php
+++ reservation-form.php
@@ -4,27 +4,22 @@ session_start() ;
 
 require('connexiondb.php') ; 
 
-if (!empty($_GET)) {
-    extract($_GET);
+if (!empty($_POST)) {
+    extract($_POST);
     $valid = (boolean) true;        // UTILISATION DE LA VARIABLE VALIDE POUR RENDRE LES ERREURS 
 
 
-    // SIMPLIFICATION DES GETS
+    // SIMPLIFICATION DES POSTS
 
-    $title = $_GET['title'];
-    $description = $_GET['description'];
-    $day = $_GET['day'];
-    $hour = $_GET['hour'];
+    $login = $_POST['login'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $day = $_POST['day'];
+    $hour = $_POST['hour'];
 
     // RESERVER => VERIF DES ERREURS
 
-    if (isset($_GET['reserver'])) {
-
-        // REQUETE POUR VOIR SI TITRE EXISTANT --> VOIR AVEC VALENTIN SI COHERENT/UTILE DE FAIRE CETTE REQUETE
-
-        // $testtitle = mysqli_query($mysqli, "SELECT * FROM reservation-salles WHERE titre = $title") ;
-        
-        // $titlerows = mysqli_num_rows($testtile) ;
+    if (isset($_POST['reserver'])) {
 
         // ERREURS TITRE
 
@@ -37,27 +32,21 @@ if (!empty($_GET)) {
         elseif (strlen($title)<45) {
             $valid = false;
             $err_title = "Le titre ne doit pas dépasser 45 caractères.";
+            echo $err_title;
         }
-
-        // VERIF SI LE TITRE EXISTANT --> VOIR AVEC VALENTIN SI COHERENT/UTILE DE FAIRE CETTE REQUETE
-
-        // elseif ($titlerows == 1 ) {
-        //    $err_title = "Un autre évènement ayant le même titre existe déjà.";
-        //    $valid = false;
-        //    echo $err_title;
-        // }
 
         // ERREURS DESCRIPTION
 
         if (empty($description)) {
             $valid = false;
             $err_description = "Veuillez renseigner une description." ;
-            echo $err_description ;
+            echo $err_description;
         }
 
         elseif (strlen($description)<500) {
             $valid = false;
             $err_description = "La description ne doit pas dépasser 500 caractères.";
+            echo $err_description;
         }
 
 
@@ -134,7 +123,7 @@ if (!empty($_GET)) {
 
                 <div class="formbox">
 
-                    <form action="reservation-form.php" method="get" class="styleform">
+                    <form action="reservation-form.php" method="post" class="styleform">
 
                         <div><input type="text" class="basicinput" name="title" placeholder="Titre de l'évènement"></div>
 
